8e69ca4542ad5be2f8ec5dd29f4e6897be3e599e
Кирюшин Денис <dkiryushin@EN-PRO.RU>
2019-09-11 11:01:02 +0300

    file list

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/slonegd-otus-go/ftp
+
+go 1.12
+
+require (
+	github.com/jlaffaye/ftp v0.0.0-20190828173736-6aaa91c7796e
+	github.com/stretchr/testify v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import(
-	// "github.com/jlaffaye/ftp"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/jlaffaye/ftp"
 )
 
-func main(){
+func main() {
+	connection, err := ftp.Dial("localhost:21", ftp.DialWithTimeout(1*time.Second))
+	if err != nil {
+		log.Fatalf("dial failed: %s", err)
+	}
+	defer func() {
+		if err = connection.Quit(); err != nil {
+			log.Fatalf("connection quit failed: %s", err)
+		}
+	}()
 
+	err = connection.Login("username", "")
+	if err != nil {
+		log.Fatalf("connection login failed: %s", err)
+	}
+
+	list, err := connection.List("/")
+	if err != nil {
+		log.Fatalf("connection list failed: %s", err)
+	}
+
+	fmt.Printf("%+s", list)
 }
